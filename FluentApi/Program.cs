dc4da225f477ec14ae195dca008435d514d4e2dc
Panya911 <Pavel_flyagin@icloud.com>
2015-05-02 12:18:51 +0500

    FluentApi and PerfLogger tasks

--- FluentApi/Program.cs
+++ FluentApi/Program.cs
@@ -1,30 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace FluentTask
 {
-	internal class Program
-	{
-		private static void Main()
-		{
+    class Behavior
+    {
+        private readonly List<Action> _actions;
 
-/*
-			var behaviour = new Behavior()
-				.Say("Привет мир!")
-				.UntilKeyPressed(b => b
-					.Say("Ля-ля-ля!")
-					.Say("Тру-лю-лю"))
-				.Jump(JumpHeight.High)
-				.UntilKeyPressed(b => b
-					.Say("Aa-a-a-a-aaaaaa!!!")
-					.Say("[набирает воздух в легкие]"))
-				.Say("Ой!")
-				.Delay(TimeSpan.FromSeconds(1))
-				.Say("Кто здесь?!")
-				.Delay(TimeSpan.FromMilliseconds(2000));
+        public Behavior()
+        {
+            _actions = new List<Action>();
+        }
 
-			behaviour.Execute();
-*/
+        public void Execute()
+        {
+            foreach (var action in _actions)
+                action();
+        }
+        public Behavior Say( string sentence)
+        {
+            _actions.Add(() => Console.WriteLine(sentence));
+            return this;
+        }
 
-		}
-	}
+        public Behavior UntilKeyPressed(Action<Behavior> actions)
+        {
+            _actions.Add(() =>
+            {
+                while (!Console.KeyAvailable)
+                {
+                    Thread.Sleep(100);
+                    var subBehavior = new Behavior();
+                    actions(subBehavior);
+                    subBehavior.Execute();
+                }
+                Console.ReadKey(false);
+            });
+            return this;
+        }
+
+        public Behavior Jump(JumpHeight jumpHeight)
+        {
+            _actions.Add(
+                () =>
+                {
+                    Console.WriteLine(jumpHeight == JumpHeight.High ?
+                        "Прыгнул высоко" : "прыгнул низко");
+                });
+            return this;
+        }
+
+        public Behavior Delay(TimeSpan delay)
+        {
+            _actions.Add(() => Thread.Sleep(delay));
+            return this;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var behaviour = new Behavior()
+                            .Say("Привет мир!")
+                            .UntilKeyPressed(b => b
+                                .Say("Ля-ля-ля!")
+                                .Say("Тру-лю-лю"))
+                            .Jump(JumpHeight.High)
+                            .UntilKeyPressed(b => b
+                                .Say("Aa-a-a-a-aaaaaa!!!")
+                                .Say("[набирает воздух в легкие]"))
+                            .Say("Ой!")
+                            .Delay(TimeSpan.FromSeconds(1))
+                            .Say("Кто здесь?!")
+                            .Delay(TimeSpan.FromMilliseconds(2000));
+
+            behaviour.Execute();
+
+
+        }
+    }
 }
--- PerfLogger/Program.cs
+++ PerfLogger/Program.cs
@@ -1,20 +1,46 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace PerfLogger
 {
+    class PerfLogger:IDisposable
+    {
+        private Action<int> _disposeAction;
+        private Stopwatch _stopwatch;
+
+        public PerfLogger(Action<int> action)
+        {
+            _disposeAction = action;
+            _stopwatch=new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public static PerfLogger Measure(Action<int> action)
+        {
+           return new PerfLogger(action);
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            _disposeAction(_stopwatch.Elapsed.Milliseconds);
+        }
+    }
+
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			/*
+			
 			var sum = 0.0;
 			using (PerfLogger.Measure(t => Console.WriteLine("for: {0}", t)))
 				for (var i = 0; i < 100000000; i++) sum += i;
 			using (PerfLogger.Measure(t => Console.WriteLine("linq: {0}", t)))
 				sum -= Enumerable.Range(0, 100000000).Sum(i => (double)i);
 			Console.WriteLine(sum);
-			*/
+			
 		}
 	}
 }
